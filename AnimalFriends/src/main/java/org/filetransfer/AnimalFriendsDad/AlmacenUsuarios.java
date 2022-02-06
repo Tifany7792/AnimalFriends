0cad42faf5c97c5faea599eb96703fec803aa5b8
Tifanydg92 <e.dominguezg.2017@alumnos.urjc.es>
2022-02-06 20:45:45 +0100

    version6

--- AnimalFriends/src/main/java/org/filetransfer/AnimalFriendsDad/AlmacenUsuarios.java
+++ AnimalFriends/src/main/java/org/filetransfer/AnimalFriendsDad/AlmacenUsuarios.java
@@ -1,5 +1,56 @@
 package org.filetransfer.AnimalFriendsDad;
 
-public class AlmacenUsuarios {
+import java.io.Serializable;
+import java.util.ArrayList;
 
+public class AlmacenUsuarios implements Serializable{
+	
+	private ArrayList<Usuarios> almacenU;
+	
+	public AlmacenUsuarios() {
+		this.almacenU = new ArrayList<Usuarios>();
+	}
+	
+	
+	public int numeroUsuarios() {
+		return almacenU.size();
+	}
+	
+	public boolean Visualizar(Usuarios u){
+        
+        if (almacenU.contains(u)) 
+            return true;
+        return false;
+    }
+
+
+
+	public void mostrarListaUsuarios(){
+	    System.out.println("----------------------------------------");
+	    System.out.println("Numero de usuarios registrados: " + numeroUsuarios());
+	
+	    System.out.println (toString());
+	
+	    System.out.println("---------------------------------------");
+	}
+	
+	@Override
+	public String toString(){
+	    String s = new String(); 
+	    for (Usuarios u: getAlmacenU())
+	        s += u.toString() + "\n";
+	    return s;
+	}
+	
+	
+	public ArrayList<Usuarios> getAlmacenU() {
+		return almacenU;
+	}
+	
+	
+	public void setAlmacenU(ArrayList<Usuarios> almacenU) {
+		this.almacenU = almacenU;
+	}
 }
+
+
