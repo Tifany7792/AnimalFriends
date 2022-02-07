07f5bb67c81b77cfe9a055c4730d6f810815b7c7
Jositote <ja.sebasdavila@gmail.com>
2022-02-07 17:51:58 +0100

    Creación de una lista, una tabla y edición de acceso a la web
    A partir de ahora en vez de entrar a la web usando /greeting habrá que poner /AnimalFriends

---
+++ AnimalFriends/src/main/java/org/filetransfer/AnimalFriendsDad/WebController.java
@@ -0,0 +1,53 @@
+package org.filetransfer.AnimalFriendsDad;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Arrays;
+import java.util.List;
+
+
+@Controller
+public class WebController {
+	private Logger log = LoggerFactory.getLogger(WebController.class);
+	@GetMapping("/AnimalFriends")
+	public String page(Model model) {
+		
+		log.trace("A TRACE Message");
+		log.debug("A DEBUG Message");
+		log.info("An INFO Message");
+		log.warn("A WARN Message");
+		log.error("An ERROR Message");
+		
+		//prueba de asignacion de variables al html
+		model.addAttribute("name", "Mundo");
+		//prueba de asignacion de booleanos a html
+		model.addAttribute("hello", true);
+		
+		//prueba de creacion de una lista de animales
+		List<String> animales = Arrays.asList("Perro","Gato","Erizo");
+		model.addAttribute("animales", animales);
+		
+		 
+		return "web_template";
+	}
+}
+
+/*
+@Controller
+public class GreetingController {
+
+	@GetMapping("/greeting")
+	public String greeting(Model model) {
+
+		model.addAttribute("name", "Mundo");
+
+		return "greeting_template";
+	}
+
+}
+*/
