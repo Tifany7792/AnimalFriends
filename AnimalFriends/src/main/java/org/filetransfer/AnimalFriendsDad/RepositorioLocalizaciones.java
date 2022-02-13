a2f8e4df866db46128340c88eb55ea1636634f8c
Tifanydg92 <e.dominguezg.2017@alumnos.urjc.es>
2022-02-13 12:14:11 +0100

    repositorios

--- AnimalFriends/src/main/java/org/filetransfer/AnimalFriendsDad/RepositorioLocalizaciones.java
+++ AnimalFriends/src/main/java/org/filetransfer/AnimalFriendsDad/RepositorioLocalizaciones.java
@@ -1,5 +1,6 @@
 package org.filetransfer.AnimalFriendsDad;
 import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 
--- AnimalFriends/src/main/java/org/filetransfer/AnimalFriendsDad/RepositorioUsuarios.java
+++ AnimalFriends/src/main/java/org/filetransfer/AnimalFriendsDad/RepositorioUsuarios.java
@@ -1,6 +1,7 @@
 package org.filetransfer.AnimalFriendsDad;
 
 import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 
