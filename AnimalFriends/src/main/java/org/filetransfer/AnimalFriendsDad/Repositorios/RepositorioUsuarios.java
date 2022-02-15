79f1d5e2bc4aab9c20a06dd206af5cccada643db
Jositote <ja.sebasdavila@gmail.com>
2022-02-15 13:53:53 +0100

    Revert "Revert "Intento 1 html usuarios""
    This reverts commit c2810c36718c402bef8a777d562a90e56264e7e0.

--- AnimalFriends/src/main/java/org/filetransfer/AnimalFriendsDad/Repositorios/RepositorioUsuarios.java
+++ AnimalFriends/src/main/java/org/filetransfer/AnimalFriendsDad/Repositorios/RepositorioUsuarios.java
@@ -4,11 +4,12 @@ import java.util.List;
 
 import org.filetransfer.AnimalFriendsDad.Entidades.Usuarios;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 
 
-public interface RepositorioUsuarios extends JpaRepository<Usuarios,Long>{
+public interface RepositorioUsuarios extends CrudRepository<Usuarios,Long>{
 	
 
 	List<Usuarios> findByNombre(String nombre);
-	List<Usuarios> findByPasword(String pasword);
+	//List<Usuarios> findByPasword(String pasword);
 }
