b8c4e2f07ca7c840778a1abe9daeb1fd7fc65794
Jose Antonio Sebastián <72799828+Jositote@users.noreply.github.com>
2022-02-15 13:52:05 +0100

    vacio

--- AnimalFriends/src/main/java/org/filetransfer/AnimalFriendsDad/Usuarios.java
+++ AnimalFriends/src/main/java/org/filetransfer/AnimalFriendsDad/Usuarios.java
@@ -4,15 +4,6 @@ package org.filetransfer.AnimalFriendsDad;
 package org.filetransfer.AnimalFriendsDad.Entidades;
 <<<<<<< HEAD:AnimalFriends/src/main/java/org/filetransfer/AnimalFriendsDad/Usuarios.java
 <<<<<<< HEAD:AnimalFriends/src/main/java/org/filetransfer/AnimalFriendsDad/Usuarios.java
-<<<<<<< HEAD:AnimalFriends/src/main/java/org/filetransfer/AnimalFriendsDad/Usuarios.java
->>>>>>> parent of a90dad4 (Intento 1 html usuarios):AnimalFriends/src/main/java/org/filetransfer/AnimalFriendsDad/Entidades/Usuarios.java
-=======
->>>>>>> parent of a90dad4 (Intento 1 html usuarios):AnimalFriends/src/main/java/org/filetransfer/AnimalFriendsDad/Entidades/Usuarios.java
-=======
->>>>>>> parent of a90dad4 (Intento 1 html usuarios):AnimalFriends/src/main/java/org/filetransfer/AnimalFriendsDad/Entidades/Usuarios.java
-=======
->>>>>>> parent of a90dad4 (Intento 1 html usuarios):AnimalFriends/src/main/java/org/filetransfer/AnimalFriendsDad/Entidades/Usuarios.java
-
 import javax.persistence.*;
 
 @Entity
