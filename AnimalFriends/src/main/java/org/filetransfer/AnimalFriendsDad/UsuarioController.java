bff4777e50f2cf1df7ba521759542f0b99b739cf
Tifanydg92 <e.dominguezg.2017@alumnos.urjc.es>
2022-02-18 12:54:02 +0100

    Quitados ya errores por conexión, último error se forma al crear la tabla DataBaseUsage

--- AnimalFriends/src/main/java/org/filetransfer/AnimalFriendsDad/UsuarioController.java
+++ AnimalFriends/src/main/java/org/filetransfer/AnimalFriendsDad/UsuarioController.java
@@ -5,7 +5,7 @@ import javax.servlet.http.HttpSession;
 import org.filetransfer.AnimalFriendsDad.Entidades.Usuarios;
 import org.filetransfer.AnimalFriendsDad.Repositorios.RepositorioUsuarios;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.context.SecurityContextHolder;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
