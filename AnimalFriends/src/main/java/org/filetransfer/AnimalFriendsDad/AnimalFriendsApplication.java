db9b14fdd0827ba0ca2f33a04f6007b9456a5999
Tifanydg92 <e.dominguezg.2017@alumnos.urjc.es>
2022-02-22 00:33:02 +0100

    UML ACTUALIZADO

---
+++ AnimalFriends/src/main/java/org/filetransfer/AnimalFriendsDad/AnimalFriendsApplication.java
@@ -0,0 +1,14 @@
+package org.filetransfer.AnimalFriendsDad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimalFriendsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnimalFriendsApplication.class, args);
+		
+	}
+
+}
