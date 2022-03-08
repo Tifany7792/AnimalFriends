4a9489bbc1b5d07b5f0eb5b8e0ef999ddfe094f8
Jositote <ja.sebasdavila@gmail.com>
2022-03-08 13:47:08 +0100

    Deshabilitado csrf

--- AnimalFriends/src/main/java/org/filetransfer/AnimalFriendsDad/Security/SecurityConfiguration.java
+++ AnimalFriends/src/main/java/org/filetransfer/AnimalFriendsDad/Security/SecurityConfiguration.java
@@ -64,7 +64,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
 		http.logout().logoutSuccessUrl("/");
 		
 		// Disable CSRF at the moment
-		//http.csrf().disable();
+		http.csrf().disable();
 		// Allow H2 console
 		http.authorizeRequests().antMatchers("/h2-console/**").permitAll();
 		http.headers().frameOptions().sameOrigin();
