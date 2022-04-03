de51ee3b89b70b60d436bfb0a1c4058d4b4c1e72
Tifanydg92 <e.dominguezg.2017@alumnos.urjc.es>
2022-04-03 20:45:40 +0200

    intento arreglo base de datos

--- AnimalFriends/src/main/java/org/filetransfer/AnimalFriendsDad/Repositorios/RepositorioProductos.java
+++ AnimalFriends/src/main/java/org/filetransfer/AnimalFriendsDad/Repositorios/RepositorioProductos.java
@@ -1,12 +1,17 @@
 package org.filetransfer.AnimalFriendsDad.Repositorios;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.filetransfer.AnimalFriendsDad.Entidades.Productos;
+import org.filetransfer.AnimalFriendsDad.Entidades.Usuarios;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface RepositorioProductos extends JpaRepository<Productos,Long>{
 	Optional<Productos> findByTipo(String tipo);
 	Optional<Productos> findById(Long id);
 	Optional<Productos> findByNombre(String nombre);
+	List<Productos> findByUsuario(Usuarios user);
 }
